a8e95e17353cd0218638eaebb5ad67fb86938e7e
Crystal.L <crystal.l0815@gmail.com>
2019-11-19 02:30:19 -0800

    Add theme

--- client/src/Main.js
+++ client/src/Main.js
@@ -2,11 +2,14 @@ import React from "react";
 import { MuiThemeProvider } from "@material-ui/core";
 import { BrowserRouter, Route } from "react-router-dom";
 
+import { theme } from "./themes/theme";
 import Login from "./pages/Login";
+
+import "./Main.css";
 
 const Main = () => {
   return (
-    <MuiThemeProvider>
+    <MuiThemeProvider theme={theme}>
       <BrowserRouter>
         <Route path="/login" component={Login} />
       </BrowserRouter>
