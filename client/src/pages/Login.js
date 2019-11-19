2b682ec4088d764e4398fbc4ddd4a73466941e9c
Crystal.L <crystal.l0815@gmail.com>
2019-11-19 02:29:52 -0800

    Add CSS to login container

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,28 +1,57 @@
 import React, { useState }  from "react";
+
 import { makeStyles, withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
+import { Typography } from "@material-ui/core";
+import { Grid } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles(theme => ({
   textField: {
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
-    width: 200,
+    boxShadow: "0px 0px 2px 0px rgb(175, 173, 179)",
+    borderRadius: 5,
+    width: "95%",
+    margin: 12,
+    textIndent: 10,
+    padding: 5,
+    height: 45,
   },
-  margin: {
-    margin: theme.spacing(1),
+  button: {
+    margin: 40,
   },
+  loginText: {
+    fontSize: 35,
+    color: "black",
+    textAlign: "center",
+    padding: 30,
+  }
 }));
 
-const ColorButton = withStyles(theme => ({
+const SubmitButton = withStyles(theme => ({
   root: {
     color: theme.palette.getContrastText('#ff0000'),
     background: "linear-gradient(45deg, #2AA897 10%, #4FBE75 90%)",
     '&:hover': {
       backgroundColor: "#ff0000",
     },
+    width: 150,
+    height: 50,
+    fontSize: 15,
   },
 }))(Button);
+
+const LoginContainer = withStyles(theme => ({
+  root: {
+    backgroundColor: "white",
+    width: 500,
+    height:400,
+    borderRadius: 10,
+    marginTop: 200,
+  }
+}))(Container);
 
 const Login = () => {
   const classes = useStyles();
@@ -31,39 +60,51 @@ const Login = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert('Submitted')
-}
+    alert('submitted!');
+  }
 
   return (
-    <ValidatorForm
-      onSubmit={handleSubmit}
-    >
-      <TextValidator
-        className={classes.textField}
-        label="Email"
-        onChange={e => handleEmail(e.target.value)}
-        name="email"
-        value={email}
-        validators={['required', 'isEmail']}
-        errorMessages={['this field is required', 'email is not valid']}
-      />
-      <TextValidator
-        className={classes.textField}
-        label="Password"
-        onChange={e => handlePassword(e.target.value)}
-        name="password"
-        type="password"
-        value={password}
-        validators={['required', 'maxLength: 6']}
-        errorMessages={['this field is required']}
-      />
-      <ColorButton 
-        variant="contained"
-        color="primary"
-        className={classes.margin}>
-        Submit
-      </ColorButton>
-    </ValidatorForm>
+    <LoginContainer maxWidth="sm">
+    <Typography className={classes.loginText}>Login</Typography>
+      <ValidatorForm
+        onSubmit={handleSubmit}
+      >
+        <TextValidator
+          className={classes.textField}
+          label="Email"
+          onChange={e => handleEmail(e.target.value)}
+          value={email}
+          validators={['required', 'isEmail']}
+          errorMessages={['this field is required', 'email is not valid']}
+          InputProps={{
+            disableUnderline: true
+         }}
+
+        />
+        <br />
+        <TextValidator
+          className={classes.textField}
+          label="Password"
+          onChange={e => handlePassword(e.target.value)}
+          type="password"
+          value={password}
+          validators={['required']}
+          errorMessages={['this field is required']}
+          InputProps={{
+            disableUnderline: true
+         }}
+        />
+        <Grid align="center">
+          <SubmitButton 
+            type="submit"
+            variant="contained"
+            color="primary"
+            className={classes.button}>
+            Submit
+          </SubmitButton>
+        </Grid>
+      </ValidatorForm>
+    </LoginContainer>
   )
 }
 export default Login;
