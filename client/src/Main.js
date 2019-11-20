bf74b9163ac43b95e110e2d0df9f2010d4121cca
Crystal.L <crystal.l0815@gmail.com>
2019-11-19 20:28:43 -0800

    Add sign up route

--- client/src/Main.js
+++ client/src/Main.js
@@ -4,14 +4,17 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import { theme } from "./themes/theme";
 import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 import "./Main.css";
 
 const Main = () => {
+  
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
         <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
       </BrowserRouter>
     </MuiThemeProvider>
   );
