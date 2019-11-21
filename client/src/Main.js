e9ef3b1a776bcff16709e774535ff1d293bf7c2f
Crystal.L <crystal.l0815@gmail.com>
2019-11-20 19:58:28 -0800

    Implement "/" to redirect to signup page

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
 
 import { theme } from "./themes/theme";
 import Login from "./pages/Login";
@@ -13,6 +13,9 @@ const Main = () => {
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
+      <Route exact path="/">
+        <Redirect to="/login" />
+      </Route>
         <Route path="/login" component={Login} />
         <Route path="/signup" component={SignUp} />
       </BrowserRouter>
