aa0e5b79bf47b790479dee6230c519c31d42dc70
YangengChen <yangengchen@gmail.com>
2019-11-21 15:34:11 -0500

    Updated prospects model

--- server/models/ProspectModel.py
+++ server/models/ProspectModel.py
@@ -9,7 +9,7 @@ class ProspectModel(db.Model):
     __tablename__ = 'prospects'
 
     id = db.Column(db.Integer, primary_key = True)
-    email = db.Column(db.String(120), unique = True, nullable = False)
+    email = db.Column(db.String(120), nullable = False)
     name = db.Column(db.String(120))
     status = db.Column(db.String(120))
     owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
