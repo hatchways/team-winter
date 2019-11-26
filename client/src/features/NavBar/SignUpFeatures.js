edb12b8a3824c5c4057e2f43448c782f5c75e07c
Crystal.L <crystal.l0815@gmail.com>
2019-11-25 22:17:24 -0800

    Code cleanup

--- client/src/features/NavBar/SignUpFeatures.js
+++ client/src/features/NavBar/SignUpFeatures.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import OutlinedButton from '../OutlinedButton';
