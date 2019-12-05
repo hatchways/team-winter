006e6c858acbe4a71ae89500518c93f8957baded
Crystal.L <crystal.l0815@gmail.com>
2019-12-04 18:01:30 -0800

    Create route to get all imported_from

--- client/src/features/SidePanel.js
+++ client/src/features/SidePanel.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import ExpandPanel from './ExpandPanel';
 
+import { getJWT } from '../utils';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -14,15 +16,58 @@ const useStyles = makeStyles(() => ({
   }
 }));
 
-const SidePanel = ({data, handleSearch, filterTerms}) => {
+const SidePanel = ({ handleSearch, filterTerms}) => {
   const classes = useStyles();
+  const [importedFromList, handleImportedFrom] = useState([{}]);
+  const [placeholderValue, handlePlaceHolder] = useState('')
 
-  const actionType1 = 'Imported from : '
+  console.log('sidepanel', importedFromList)
+  const actionType1 = 'Imported from : ';
+
+  const getAllImportedFrom = () => {
+    fetch(`/prospects`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${getJWT()}`
+      }
+    })
+    .then(res => res.json())
+      .then(result => {
+        const importedFromObj = {};
+        let idx = 0;
+
+        result.Prospects.map((prospect) => {
+          if (!importedFromObj[prospect.imported_from]) {
+            importedFromObj[prospect.imported_from] = {'name': prospect.imported_from, 'id': idx};
+            idx ++;
+          } 
+        })
+        handleImportedFrom(Object.values(importedFromObj));
+      })
+    .catch(err => {
+      console.log(err.message);
+    });
+  }
+
+  const handleQueryTerm = (query) => {
+    const queried = (importedFromList.filter(each => (each.id === query))[0].name)
+    handleSearch(queried)
+    handlePlaceHolder(queried)
+  }
 
   return (
     <Paper className={classes.root}>
     <div className={classes.test}></div>
-      <ExpandPanel actionType={actionType1} data={data} handleSearch={handleSearch} filterTerms={filterTerms}></ExpandPanel>
+      <ExpandPanel
+        actionType={actionType1}
+        importedFromList={importedFromList}
+        handleSearch={handleSearch}
+        filterTerms={filterTerms}
+        getAllImportedFrom={getAllImportedFrom}
+        placeholderValue={placeholderValue}
+        handleQueryTerm={handleQueryTerm}
+        >
+      </ExpandPanel>
     </Paper>
   );
 }
