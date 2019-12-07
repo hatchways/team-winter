176c4fcdcbca231abe9176607100354f0b1e8e83
YangengChen <yangengchen@gmail.com>
2019-12-07 12:15:17 -0500

    Merge branch 'dev' of https://github.com/hatchways/team-winter into Send-emails-using-Gmail-api

--- client/src/pages/Prospects.js
+++ client/src/pages/Prospects.js
@@ -1,23 +1,25 @@
-
-
 import React, { Fragment, useState, useEffect } from 'react';
+
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import queryString from 'query-string';
-
+import Grid from '@material-ui/core/Grid';
 import CloudIcon from '@material-ui/icons/Cloud';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import MailIcon from '@material-ui/icons/Mail';
 import FlashOnIcon from '@material-ui/icons/FlashOn';
+
 import CustomizedButton from '../features/CustomizedButton';
 import OutlinedButton from '../features/OutlinedButton';
 import NavBar from '../features/NavBar/MainBody';
 import UserInputContainer from '../features/UserInputContainer';
 import DataTable from '../features/DataTable';
-import CustomizedDialog from '../features/CustomizedDialog'
+import AddToCampaignDialog from '../features/AddToCampaignDialog';
 import GmailDialog from '../features/GmailDialog';
+import GmailAuthorizationHandler from '../features/GmailAuthorizationHandler';
 import { getJWT } from '../utils';
+import SidePanel from '../features/SidePanel';
 
 const useStyles = makeStyles((theme) => ({
   importButton: {
@@ -41,7 +43,7 @@ const useStyles = makeStyles((theme) => ({
     margin: "16px 17px 0px 14px",
   },
   featuresContainer: {
-    padding: "100px 60px 0px",
+    padding: "100px 35px 0px",
     height: 105,
   },
   titleContainer: {
@@ -57,38 +59,42 @@ const useStyles = makeStyles((theme) => ({
       paddingRight: 15,
     },
     [theme.breakpoints.down("md")]: {
-      paddingLeft: 0,
-      paddingRight: 0,
+      paddingLeft: 10,
+      paddingRight: 10,
     },
     [theme.breakpoints.down("sm")]: {
-      paddingLeft: 0,
-      paddingRight: 0,
+      paddingLeft: 5,
+      paddingRight: 5,
     }
+  },
+  tableContainer: {
+    width: '95%'
   }
 }));
 
 const Prospects = () => {
   const classes = useStyles();
-  const actionType = 'Add to Campaign'
+  const actionType = ['Add to Campaign']
 
   const [data, handleData] = useState([{}]);
   const [dialog, handleDialog] = useState(null);
   const [listOfCampaigns, handleCampaigns] = useState(null);
   const [campaignId, setCampaignId] = useState('');
   const [selectedProspects, handleSelectedProspects] = useState([]);
+  const [importedFromTerm, handleSearchImportedFrom] = useState({id: '', name: ''});
+  const [statusTerm, handleSearchStatus] = useState({id: '', name: ''});
+  const [emailTerm, handleSearchEmail] = useState('');
 
   useEffect(() => {
     getAllCampaigns();
     getAllProspects();
-  }, [])
-
+  },[])
 
   const gmailDialogShouldOpen = () => {
     const qs = queryString.parse(window.location.search);
     if(qs['gmail_dialog']) return true;
     return false;
   }
-
 
   const getAllProspects = () => {
     fetch(`/prospects`, {
@@ -108,24 +114,18 @@ const Prospects = () => {
             'check': 'check',
             'Email': prospect.email,
             cloudIcon,
-            'Status': 'working',
+            'Status': prospect.status,
             'Owner': prospect.name,
             'Campaigns': prospect.campaigns,
-            'Last Contacted': prospect.lastContacted,
-            'Emails...': prospect.emails,
-            'imported_from': prospect.imported_from
+            'Imported_from': prospect.imported_from
           }
-          return listOfProspects.push(prospectObj)
-        })
-        handleData(listOfProspects)
+          return listOfProspects.push(prospectObj);
+        });
+        handleData(listOfProspects);
       })
     .catch(err => {
       console.log(err.message);
     });
-  }
-
-  const handleSearch = () => {
-    
   }
 
   const getAllCampaigns = () => {
@@ -137,7 +137,7 @@ const Prospects = () => {
     })
     .then(res => res.json())
       .then(data => {
-        handleCampaigns(data.Campaigns)
+        handleCampaigns(data.Campaigns);
     })
     .catch(err => {
       console.log(err.message);
@@ -145,8 +145,8 @@ const Prospects = () => {
   }
 
   const handleCloseDialogAndSaveProspects = () => {
-    handleDialog(false)
-    saveProspectsToCampaign()
+    handleDialog(false);
+    saveProspectsToCampaign();
   } 
 
   const saveProspectsToCampaign = () => {
@@ -201,78 +201,112 @@ const Prospects = () => {
     handleSelectedProspects(newSelected);
   };
 
-  const propsForDialog = {
-    actionType,
-    dialog,
-    listOfCampaigns,
-    setCampaignId,
-    campaignId,
-    handleCloseDialogAndSaveProspects,
-    handleDialog
-  }
+  // Filter inported_from's option
+  const filteredImportedFrom = importedFromTerm.name === ''
+  ? data 
+  : data.filter(data => data['Imported_from'] === importedFromTerm.name);
+ 
+  // Filter status's option
+  const filteredStatus = statusTerm.name === ''
+  ? filteredImportedFrom
+  : filteredImportedFrom.filter(data => data['Status'] === statusTerm.name);
+
+   // Filter email's option
+  const filteredEmail = emailTerm === ''
+  ? filteredStatus
+  : filteredStatus.filter(data => {
+    const queryEmail = emailTerm.toLowerCase();
+    return data['Email'].includes(queryEmail);
+  })
 
   const propsForDataTable = {
-    data,
+    data: filteredEmail,
     handleClickOnAllRows,
     handleClickOnRow,
     selectedProspects,
   }
-  
+
   return (
     <Fragment>
       <NavBar />
-      <div>
-        <Box
-          className={classes.featuresContainer}
-          display="flex">
-          <Box flexGrow={1}>
-            <Box className={classes.titleContainer}>
-              <Typography variant="h5"> Prospects </Typography>
+      <Grid container id="container">
+        <Grid item lg={2} id='sidePanel' className="halfContainer">
+          <Box>
+          <SidePanel
+          importedFromTerm={importedFromTerm}
+          handleSearchImportedFrom={handleSearchImportedFrom}
+          statusTerm={statusTerm}
+          handleSearchStatus={handleSearchStatus}
+          emailTerm={emailTerm}
+          handleSearchEmail={handleSearchEmail}
+          > </SidePanel>
+          </Box>
+        </Grid>
+        <Grid item lg={10} className="halfContainer">
+          <Box id='ContainerWrapper' display="flex" flexDirection="row" justifyContent="center">
+            <Box id="FeatureContainerAndDataTable" display="flex" flexDirection="column" width='100%'>
+              <Box id="FeaturesContainer">
+                  <Box
+                  className={classes.featuresContainer}
+                  display="flex">
+                  <Box flexGrow={1}>
+                    <Box className={classes.titleContainer}>
+                      <Typography variant="h5"> Prospects </Typography>
+                    </Box>
+                    <Box>
+                      {selectedProspects.length > 0 &&
+                      <CustomizedButton
+                        onClick={() => handleDialog(true)}>
+                        {actionType}
+                      </CustomizedButton>}
+                    </Box>
+                  </Box>
+                    {dialog === true &&
+                      <AddToCampaignDialog
+                      actionType={actionType}
+                      dataList={listOfCampaigns}
+                      setValue={setCampaignId}
+                      currentValue={campaignId}
+                      dialog={dialog}
+                      handleCloseDialogAndSaveProspects={handleCloseDialogAndSaveProspects}
+                      handleDialog={handleDialog}
+                      />}
+                  <Box className={classes.icon}>
+                    <FlashOnIcon fontSize="small" style={{color: "grey"}} />
+                  </Box>
+                  <Box className={classes.icon}>
+                    <MailIcon fontSize="small" style={{color: "grey"}} />
+                  </Box>
+                  <Box pl={2}>
+                    <OutlinedButton className={classes.importButton}> Imports </OutlinedButton>
+                  </Box>
+                  <Box pl={1}>
+                    <CustomizedButton 
+                      className={classes.newProspectButton}>
+                      Add New Prospect
+                      <div className={classes.seperationLine}></div>
+                      <div className={classes.arrow} >
+                        <ArrowDropDownIcon fontSize="small" style={{color: "white"}} pt={3} />
+                      </div>
+                    </CustomizedButton>
+                  </Box>
+                </Box>
+              </Box>
+              <Box display="flex" justifyContent="center" id="DataTable" >
+              <Box className={classes.tableContainer}>
+                <UserInputContainer className={classes.prospectList}>
+                  <DataTable props={propsForDataTable}></DataTable>
+                </UserInputContainer>
             </Box>
-            <Box>
-            {selectedProspects.length > 0 &&
-              <CustomizedButton
-                onClick={() => handleDialog(true)}>
-                {actionType}
-              </CustomizedButton>}
+              </Box>
             </Box>
           </Box>
-            {dialog === true &&
-              <CustomizedDialog
-              props={propsForDialog}
-              />}
-          <Box className={classes.icon}>
-            <FlashOnIcon fontSize="small" style={{color: "grey"}} />
-          </Box>
-          <Box className={classes.icon}>
-            <MailIcon fontSize="small" style={{color: "grey"}} />
-          </Box>
-          <Box pl={2}>
-            <OutlinedButton className={classes.importButton}> Imports </OutlinedButton>
-          </Box>
-          <Box pl={1}>
-            <CustomizedButton 
-              className={classes.newProspectButton}>
-              Add New Prospect
-              <div className={classes.seperationLine}></div>
-              <div className={classes.arrow} >
-                <ArrowDropDownIcon fontSize="small" style={{color: "white"}} pt={3} />
-              </div>
-            </CustomizedButton>
-          </Box>
-        </Box>
-      </div>
-      <Box className="classes.tagsContainer" display="flex" justifyContent="center">
-      </Box>
-      <UserInputContainer className={classes.prospectList}>
-        <DataTable
-          props={propsForDataTable}
-          >
-        </DataTable>
-      </UserInputContainer>
-      <GmailDialog open={gmailDialogShouldOpen()} />
+        </Grid>
+      </Grid>
+      <GmailDialog open={gmailDialogShouldOpen()} /> 
+      <GmailAuthorizationHandler/>
     </Fragment>
   )
 }
 
-export default Prospects
+export default Prospects;
