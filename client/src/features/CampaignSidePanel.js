9200b5e960c57a14687ecba096eeac12ae1531e0
Crystal.L <crystal.l0815@gmail.com>
2019-12-10 23:20:01 -0800

    Implement summary view and prospect view

---
+++ client/src/features/CampaignSidePanel.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect, Fragment } from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import LensIcon from '@material-ui/icons/FiberManualRecord';
+
+import CustomizedButton from './CustomizedButton';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    height: 685,
+    paddingTop: 200,
+  },
+  lensIcon: {
+    color: "#a5d6a7",
+    fontSize: 10,
+    padding: 10,
+  },
+  activeButton: {
+    margin: 20,
+    width: 170,
+    height: 40,
+    justifyContent: "flex-start",
+    borderRadius: 10,
+  },
+  unactiveButton: {
+    margin: "20px 50px",
+    width: 170,
+    height: 40,
+    justifyContent: "flex-start",
+    '&:hover': {
+      background: "none"
+    }
+  }
+}));
+
+const CampaignSidePanel = ({currentView, setCurrentViewToSummary, setCurrentViewToProspects}) => {
+  const classes = useStyles();
+
+  let display = null;
+  if (currentView === 'summary') {
+    display = (
+      <Fragment>
+        <CustomizedButton className={classes.activeButton}> <LensIcon className={classes.lensIcon} /> Summary </CustomizedButton>
+        <Button component="span" className={classes.unactiveButton} onClick={() => setCurrentViewToProspects()}>Prospects</Button>
+      </Fragment>
+    )
+  }
+  
+  if (currentView === 'prospects') {
+    display = (
+      <Fragment>
+        <Button component="span" className={classes.unactiveButton} onClick={() => setCurrentViewToSummary()}>Summary</Button>
+        <CustomizedButton className={classes.activeButton}> <LensIcon className={classes.lensIcon} /> Prospects </CustomizedButton>
+      </Fragment>
+    )
+  }
+ 
+  return (
+    <Paper className={classes.root}>
+      {display}
+    </Paper>
+  );
+}
+
+export default CampaignSidePanel;
