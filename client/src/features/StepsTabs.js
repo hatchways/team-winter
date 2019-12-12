de54d3bbf80ed6ec262076bf4adb2367fc34d643
Crystal.L <crystal.l0815@gmail.com>
2019-12-12 02:51:11 -0800

    Implement scrollable container for steps tabs

--- client/src/features/StepsTabs.js
+++ client/src/features/StepsTabs.js
@@ -6,22 +6,24 @@ import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    overflowX: "auto",
+    overflow: 'hidden',
     border: `1px solid ${theme.palette.divider}`,
-    borderRadius: '5px',
+    borderRadius: '15px',
     backgroundColor: theme.palette.background.paper,
     color: theme.palette.text.secondary,
-    height: 60,
+    height: 64,
     margin: "30px 0px",
+
   },
   tab: {
     height: 60,
-    borderRight: "2px solid #EDECF2 "
+    borderRight: "2px solid #EDECF2 ",
+
   },
   selected: {
     background: "linear-gradient(45deg, #2AA897 20%, #4FBE75 80%)",
     height: 60,
-  }
+  },
 }))
 
 const StepsTabs = (props) => {
@@ -34,20 +36,28 @@ const StepsTabs = (props) => {
     setValue(newValue);
   };
 
+
   return (
     <Grid container alignItems="center" className={classes.root}>
       <Tabs
+        variant="scrollable"
+        scrollButtons="auto"
         value={value}
         onChange={handleChange}
-        variant="fullWidth"
         TabIndicatorProps={{ style: { background: 'none' } }}
       >
+        <Tab
+          className={(0 === selected) ? classes.selected : classes.tab}
+          key={0}
+          label={"All"}
+          onClick={() => setSelected(0)}
+        />
         {steps.map((tab, idx) => (
           <Tab
-            className={(idx === selected) ? classes.selected : classes.tab}
-            key={idx}
-            label={`Step ${idx}`}
-            onClick={() => setSelected(idx)}
+            className={(idx + 1 === selected) ? classes.selected : classes.tab}
+            key={idx + 1}
+            label={`Step ${ idx + 1 }`}
+            onClick={() => setSelected(idx + 1)}
             />
         ))}
       </Tabs>
