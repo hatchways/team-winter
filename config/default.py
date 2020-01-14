0dc94b8990fd1a129b89f343eb94df539605cf8a
YangengChen <yangengchen@gmail.com>
2020-01-14 13:39:54 -0500

    Update default config file

--- config/default.py
+++ config/default.py
@@ -8,4 +8,7 @@ JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(30)
 WTF_CSRF_ENABLED = True
 REDIS_URL = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://postgres:1234@localhost/team-winter')
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
