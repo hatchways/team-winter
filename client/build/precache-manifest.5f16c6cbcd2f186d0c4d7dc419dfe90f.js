38e731279332ba87d4adda0a7f73496c7b079b15
YangengChen <yangengchen@gmail.com>
2020-01-17 17:11:27 -0500

    Updated .gitignore

---
+++ client/build/precache-manifest.5f16c6cbcd2f186d0c4d7dc419dfe90f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f6c0321ef06172e381d09b5d4966c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd1a5d6f722d0a2038fa",
+    "url": "/static/css/main.4a8d6559.chunk.css"
+  },
+  {
+    "revision": "1129b6966cee1b5a3693",
+    "url": "/static/js/2.73e8dacc.chunk.js"
+  },
+  {
+    "revision": "fd1a5d6f722d0a2038fa",
+    "url": "/static/js/main.1d2f3b0e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
